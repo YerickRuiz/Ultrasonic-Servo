25c076e97699ca5b091f355bfdf626b8a5f0dc80
maiikki <maikki.2001@gmail.com>
2022-10-02 14:35:32 +0200

    Ultrasound script uploaded

--- ultrasonic-servo/ultrasonic-servo.ino
+++ ultrasonic-servo/ultrasonic-servo.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+const int us_trigger = 2;
+const int us_echo = 3;
 
+int us_get_distance() {
+  int distance;
+  long duration;
+
+  digitalWrite(us_trigger, HIGH);
+  delay(100);
+  digitalWrite(us_trigger, LOW);
+
+  duration = pulseIn(us_echo, HIGH);
+  distance = duration * 0.034 / 2;
+
+  return distance;
+}
+
+
+
+void setup() {
+  pinMode(us_trigger, OUTPUT);
+  pinMode(us_echo, INPUT);
+
+  Serial.begin(9600);
+  digitalWrite(us_trigger, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int distance = us_get_distance();
 
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println("cm");
+
+  //Send distance to servo
 }
